38de65514a55f74528f69889913393f58c3cdcce
IsabellaBrander <isabellacaterina.brander01@estudiant.upf.edu>
2024-10-20 18:58:16 +0200

    fourth attempt

--- src/antiprime.py
+++ src/antiprime.py
@@ -1,31 +1,30 @@
 ## ADD WHATEVER ARGUMENTS ARE NECESSARY TO THE MAIN FUNCTION
 ## IN THE SAME ORDER AS THE ARGUMENTS ARE TAKEN FROM THE
 ## COMMAND LINE SPECIFIED BELOW
-import sys
-x = int (sys.argv [1])
+
 def main(x) :
 	## YOU CODE SHOULD START HERE AST THE SAME
 	## IDENTATION AS THIS COMMENT
 	i = 1
 	r = 0
 	while i <= x: 
-	  if x % i == 0:
-	  	r = r + 1
-	  i = i + 1
+		if x % i == 0:
+			r = r + 1
+		i = i + 1
 	l = x - 1
 	s = 0
 	while l >= 1 and s < r:
-	  a = 1
-	  s = 0
-	  while a <= l: 
-	    if l % a == 0:
-	      s = s + 1
-	    a = a +1
-	  l = l - 1
+		a = 1
+		s = 0
+		while a <= l: 
+			if l % a == 0:
+				s = s + 1
+			a = a +1
+		l = l - 1
 	if s >= r:
-	   res = "not anti-prime"
+		res = "not anti-prime"
 	else: 
-	   res = "anti-prime"
+		res = "anti-prime"
 
 	## THE LAST LINES OF YOUR CODE SHOULD EITHER
 	## RETURN THE VALUE "anti-prime" or "not anti-prime"
@@ -35,7 +34,8 @@ def main(x) :
 	return(res)
 ## DO NOT REMOVE THIS LINE BELOW
 if __name__ == "__main__" :
-
+	import sys
+	x = int (sys.argv [1])
 	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
 	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
 	## $ python antiprime.py 6
